3963ea12c5bab76027576ea74c9cd1c8315eab7d
임승현 <96limshyun@gmail.com>
2025-05-04 17:36:40 +0900

    예제 변경

--- react-deep-dive/week56/george/App.tsx
+++ react-deep-dive/week56/george/App.tsx
@@ -2,29 +2,15 @@ import { ReactNode } from "react";
 
 import { createPortal } from "@/createPortal";
 
-const Modal = () => {
-  return createPortal(
-    <div style={{ color: "red" }}>
-      <p>hello</p>
-    </div>,
-    document.body
-  ) as unknown as ReactNode;
-};
-
-const Component1 = () => {
-  return (
-    <div>component1</div>
-  );
-};
-
 const App = () => {
   return (
-    <>
-      <div>
-        <Component1/>
-        <Modal/>
-      </div>
-    </>
+    <div style={{ border: '2px solid black' }}>
+      <p>This child is placed in the parent div.</p>
+      {createPortal(
+        <p>This child is placed in the document body.</p>,
+        document.body
+      ) as unknown as ReactNode}
+    </div>
   );
 };
 
