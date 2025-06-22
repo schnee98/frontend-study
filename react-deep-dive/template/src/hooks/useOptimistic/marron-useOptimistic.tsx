b06d19ce855e1b602b7822288ce47568bebfca1e
Marron <131264106+dmdeh@users.noreply.github.com>
2025-06-22 22:13:07 +0900

    [marron] week 60 useOptimistic (#345)
    * docs: week 60
    
    * docs: marron test 파일 추가

---
+++ react-deep-dive/template/src/hooks/useOptimistic/marron-useOptimistic.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
+
+// export function useOptimistic<State>(
+//   passthrough: State
+// ): [State, (action: State | ((pendingState: State) => State)) => void];
+
+// export function useOptimistic<State, Action>(
+//   passthrough: State,
+//   reducer: (state: State, action: Action) => State
+// ): [State, (action: Action) => void];
+
+function useCustomOptimistic<State, Action>(
+  passthrough: State,
+  reducer?: (state: State, action: Action) => State
+): [State, (action: Action) => void] {
+  const [state, setState] = useState(passthrough);
+  useEffect(() => setState(passthrough), [passthrough]);
+
+  const dispatch = useCallback(
+    (action: any) => {
+      if (reducer) {
+        setState((prev) => reducer(prev, action));
+      } else {
+        setState((prev) =>
+          typeof action === "function" ? action(prev) : action
+        );
+      }
+    },
+    [reducer]
+  );
+  return [state, dispatch];
+}
+export default useCustomOptimistic;
