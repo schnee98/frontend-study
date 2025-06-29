e81c42383ca6d50d84bd46b067ea6fc6cc990780
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-06-29 22:22:51 +0900

    feat: curry

---
+++ lodash/src/curry/marron-curry.ts
@@ -0,0 +1,29 @@
+const placeholder = Symbol("placeholder");
+
+function curry(fn: Function) {
+  return function curried(...args) {
+    const countArgs = args.filter((arg) => arg !== placeholder);
+
+    if (countArgs.length >= fn.length && !args.includes(placeholder)) {
+      const finalArgs = args.slice(0, fn.length);
+      return fn(...finalArgs);
+    }
+
+    return function (...nextArgs) {
+      let merged = [...args];
+      let nextIndex = 0;
+
+      for (let i = 0; i < merged.length && nextIndex < nextArgs.length; i++) {
+        if (merged[i] === placeholder) {
+          merged[i] = nextArgs[nextIndex++];
+        }
+      }
+      merged = [...merged, ...nextArgs.slice(nextIndex)];
+
+      return curried(...merged);
+    };
+  };
+}
+curry.placeholder = placeholder;
+
+export { curry };
