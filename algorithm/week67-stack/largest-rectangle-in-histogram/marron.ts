c61f8866e629f0804e0fdc7566fd2ad2de9c0200
Eunseo Kim <131264106+dmdeh@users.noreply.github.com>
2025-08-31 23:08:08 +0900

    feat: week 67 stack (#378)

---
+++ algorithm/week67-stack/largest-rectangle-in-histogram/marron.ts
@@ -0,0 +1,25 @@
+class Solution {
+  /**
+   * @param {number[]} heights
+   * @return {number}
+   */
+  largestRectangleArea(heights) {
+    const stack = [];
+    let maxArea = 0;
+    heights.push(0);
+
+    for (let i = 0; i < heights.length; i++) {
+      while (
+        stack.length > 0 &&
+        heights[i] < heights[stack[stack.length - 1]]
+      ) {
+        const height = heights[stack.pop()];
+        const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+        maxArea = Math.max(maxArea, height * width);
+      }
+      stack.push(i);
+    }
+
+    return maxArea;
+  }
+}
